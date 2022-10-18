7fdb375e850e8485eb0a0b1a4c9fafc133473430
Nate T <gamerkid01234@gmail.com>
2022-10-18 15:09:57 +0000

    update startup script

--- onstartup.sh
+++ onstartup.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 sudo apt-get update
-sudo apt-get install -y libsfml-dev build-essential cmake git libx11-dev freeglut3-dev libxcb-image0-dev libudev-dev libjpeg8-dev libopenal-dev libsndfile1-dev libfreetype6-dev
+sudo apt-get install -y build-essential cmake fonts-noto fonts-noto-color-emoji fonts-noto-extra
 pip3 install pynput
