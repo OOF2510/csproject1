01f93654f32df042ccddc130db3a9d2eac24be70
Nate T <gamerkid01234@gmail.com>
2022-10-18 12:47:30 +0000

    finish actual code, need to make graphics not bad

--- src/main.py
+++ src/main.py
@@ -1,38 +1,74 @@
 from tkinter import *
 from tkinter import ttk
+from pynput.mouse import Button, Controller
+from pynput import keyboard
+from time import sleep as wait
+
+mouse_control = Controller()
 
 def do_nothing():
     return
 
 def add_range(list, amount):
-    amount += 1
     for i in range(amount):
-        if (i == 0):
-            do_nothing()
-        else:
-            list.append(i)
+        list.append(i)
 
 seconds = []
-add_range(seconds, 60)
+add_range(seconds, 26)
+mouse_buttons = ["Left Click", "Left Click", "Middle Click", "Right Click"]
+buttonmap = {
+    "Left Click": Button.left,
+    "Middle Click": Button.middle,
+    "Right Click": Button.right
+}
 
-def autoclick(ms, mousebutton):
-    return print(f"{ms}, {mousebutton}")
+endkey_pressed = False
+def on_release(key):
+    if key == keyboard.Key.esc:
+        global endkey_pressed
+        endkey_pressed = True
+
+listener = keyboard.Listener(on_release=on_release)
+listener.start()
+
+running = False
+def autoclick(sec, mb):
+    mb = buttonmap[mb]
+    sec = int(sec)
+    global running
+    global endkey_pressed
+    running = True
+    endkey_pressed = False
+    print(f"Autoclicker started, press the 'esc' key twice to end")
+    while running:
+        mouse_control.press(mb)
+        mouse_control.release(mb)
+        if endkey_pressed == True:   
+            running = False
+        else:
+            wait(sec)
 
 win = Tk()
 win.title("Autoclicker")
-win.geometry('400x200')
+win.geometry('600x200')
 
 frame = ttk.Frame(win, padding=0)
 frame.grid()
 
 ttk.Label(frame, text="Autoclicker").grid(column=0, row=0)
 
-dropdown_default = StringVar(win)
-dropdown_default.set(seconds[0])
-dropdown = ttk.OptionMenu(win, dropdown_default, *seconds)
-dropdown.grid(column=1, row=0)
+ttk.Label(frame, text="Seconds in between clicks").grid(column=2, row=0)
+time_choice = StringVar(win)
+time_choice.set(seconds[1])
+time_dropdown = ttk.OptionMenu(win, time_choice, *seconds)
+time_dropdown.grid(column=2, row=1)
 
-ttk.Button(frame, text="Start", command=lambda: autoclick(2, 1)).grid(column=1, row=2)
-ttk.Button(frame, text="Quit", command=win.destroy).grid(column=1, row=3)
+mb_choice = StringVar(win)
+mb_choice.set(mouse_buttons[1])
+mb_dropdown = ttk.OptionMenu(win, mb_choice, *mouse_buttons)
+mb_dropdown.grid(column=2, row=2)
+
+ttk.Button(frame, text="Start", command=lambda: autoclick(time_choice.get(), mb_choice.get())).grid(column=1, row=3)
+ttk.Button(frame, text="Quit", command=win.destroy).grid(column=1, row=4)
 
 win.mainloop()
