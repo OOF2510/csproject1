afe999a78a9a211576d6b85e903ac150378a85ba
Nate T <gamerkid01234@gmail.com>
2022-10-19 15:16:48 +0000

    efewfewfewf

--- onstartup.sh
+++ onstartup.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 sudo apt-get update
 sudo apt-get install -y build-essential cmake fonts-noto fonts-noto-color-emoji fonts-noto-extra python3 python3-pip python3-tk
-pip3 install pynput
+pip3 install -r /workspace/csproject1/requirements.txt
