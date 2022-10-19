6bb0d4561086856f350ef54054d884c53d630044
Nate T <gamerkid01234@gmail.com>
2022-10-19 15:14:43 +0000

    update onstartup script

--- onstartup.sh
+++ onstartup.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 sudo apt-get update
-sudo apt-get install -y build-essential cmake fonts-noto fonts-noto-color-emoji fonts-noto-extra
+sudo apt-get install -y build-essential cmake fonts-noto fonts-noto-color-emoji fonts-noto-extra python3 python3-pip python3-tk
 pip3 install pynput
