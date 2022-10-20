be70510c57bcae32126d03fc11300ad7e66dd178
Nate T <gamerkid01234@gmail.com>
2022-10-20 12:03:23 +0000

    update startup script

--- onstartup.sh
+++ onstartup.sh
@@ -2,3 +2,14 @@
 sudo apt-get update
 sudo apt-get install -y build-essential cmake fonts-noto fonts-noto-color-emoji fonts-noto-extra python3 python3-pip python3-tk
 pip3 install -r /workspace/csproject1/requirements.txt
+echo "[Desktop Entry]
+Version=1.0
+Type=Application
+Name=AutoClicker
+Comment=
+Exec=python3 /workspace/csproject1/src/main.py
+Icon=application-x-executable
+Path=
+Terminal=true
+StartupNotify=false" >> /home/gitpod/Desktop/AutoClicker.desktop
+chmod +x /home/gitpod/Desktop/AutoClicker.desktop
