226d0e9221f8914c5906893fc0ab59d87771d985
Nate T <gamerkid01234@gmail.com>
2022-10-25 11:31:24 +0000

    update startup script

--- onstartup.sh
+++ onstartup.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
-sudo apt-get update
-sudo apt-get install -y build-essential cmake fonts-noto fonts-noto-color-emoji fonts-noto-extra python3 python3-pip python3-tk
-pip3 install -r /workspace/csproject1/requirements.txt
 echo "[Desktop Entry]
 Version=1.0
 Type=Application
@@ -13,3 +10,6 @@ Path=
 Terminal=true
 StartupNotify=false" > /home/gitpod/Desktop/AutoClicker.desktop
 chmod +x /home/gitpod/Desktop/AutoClicker.desktop
+sudo apt-get update
+sudo apt-get install -y build-essential cmake fonts-noto fonts-noto-color-emoji fonts-noto-extra python3 python3-pip python3-tk
+pip3 install -r /workspace/csproject1/requirements.txt
